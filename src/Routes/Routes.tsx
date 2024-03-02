cda648725e58c987f656c5e002275b99773633b9
jolley34 <erlandssonjoel@gmail.com>
2024-03-02 10:45:22 +0100

    gitgit

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import App from "../app";
 import ApiProvider from "../contexts/ApiContext";
 import Home from "../pages/HomePage/Home";
 import TransactionPage from "../pages/InsiderTransactionsPage/TransactionPage";
