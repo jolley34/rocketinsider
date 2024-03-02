eb0f514c3b980a70bf1b549f4f25ec94e53005fb
jolley34 <erlandssonjoel@gmail.com>
2024-03-02 09:41:40 +0100

    test

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import HomePage from "../../src/Pages/HomePage/HomePage";
 import App from "../App";
 import ApiProvider from "../Contexts/ApiContext";
-import HomePage from "../Pages/HomePage/HomePage";
 import TransactionPage from "../Pages/InsiderTransactionsPage/TransactionPage";
 
 export const router = createBrowserRouter([
