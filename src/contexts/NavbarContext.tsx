70f1d7c96b22b4ccfd501f9e5026c6c82edbb3e5
jolley34 <erlandssonjoel@gmail.com>
2024-03-05 01:36:30 +0100

    cmiit

---
+++ src/contexts/NavbarContext.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren, createContext, useContext, useState } from "react";
+interface NavbarContextType {
+  selectedNavItem: string | null;
+  setSelectedNavItem: (value: string | null) => void;
+}
+
+const NavbarContext = createContext<NavbarContextType>({
+  selectedNavItem: null,
+  setSelectedNavItem: () => {},
+});
+
+function NavbarContextProvider(props: PropsWithChildren<{}>) {
+  const [selectedNavItem, setSelectedNavItem] = useState<string | null>(null);
+
+  return (
+    <NavbarContext.Provider value={{ selectedNavItem, setSelectedNavItem }}>
+      {props.children}
+    </NavbarContext.Provider>
+  );
+}
+
+export const useNavbar = () => useContext(NavbarContext);
+export default NavbarContextProvider;
